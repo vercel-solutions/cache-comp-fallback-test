59d452a800f8ffe60d62f13d8aa9c94f687b9e32
darekrossman <darek@subpopular.dev>
2025-11-01 16:46:59 -0400

    sync

--- app/[lang]/post/[id]/page.tsx
+++ app/[lang]/post/[id]/page.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from "react";
 import { Post, PostFallback } from "@/components/post";
 
+export async function generateMetadata() {
+  return [{ lang: "en", id: "1" }];
+}
+
 export default async function Page({ params }: PageProps<"/[lang]/post/[id]">) {
   return (
     <article className="flex flex-col gap-4">
-      <Suspense fallback={<PostFallback />}>
-        <Post params={params} />
-      </Suspense>
+      {/* <Suspense fallback={<PostFallback />}> */}
+      <Post params={params} />
+      {/* </Suspense> */}
     </article>
   );
 }
