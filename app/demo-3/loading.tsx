9c564c5a92ce970e143b1441d334d0cdac12089e
darekrossman <darek@subpopular.dev>
2025-11-02 15:03:33 -0500

    sync

--- app/demo-3/loading.tsx
+++ app/demo-3/loading.tsx
@@ -3,7 +3,7 @@ import { Code } from "@/components/code";
 export default function Loading() {
   return (
     <div className="flex justify-center items-center h-svh bg-green-500 text-white">
-      <Code>/demo-3loading.tsx</Code>
+      <Code>/demo-3/loading.tsx</Code>
     </div>
   );
 }
