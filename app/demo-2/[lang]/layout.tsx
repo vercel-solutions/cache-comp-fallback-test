78b92d64ec74732ae1565df842c111754dd4c843
darekrossman <darek@subpopular.dev>
2025-11-02 17:26:22 -0500

    sync

--- app/demo-2/[lang]/layout.tsx
+++ app/demo-2/[lang]/layout.tsx
@@ -1,5 +1,4 @@
-import "@/app/globals.css";
-import { Nav } from "@/components/nav";
+import { DemoLayout } from "@/components/demo-layout";
 
 export default async function Demo2LangLayout({
   children,
@@ -10,22 +9,7 @@ export default async function Demo2LangLayout({
   return (
     <html lang={lang}>
       <body className="flex justify-center items-center h-svh">
-        <div className="flex max-md:flex-col justify-center items-stretch max-w-4xl relative mx-auto border border-dashed border-neutral-800">
-          <div className="absolute top-0 left-0 size-10 -m-px border-t border-l border-neutral-700" />
-          <div className="absolute top-0 right-0 size-10 -m-px border-t border-r border-neutral-700" />
-          <div className="absolute bottom-0 left-0 size-10 -m-px border-b border-l border-neutral-700" />
-          <div className="absolute bottom-0 right-0 size-10 -m-px border-b border-r border-neutral-700" />
-
-          {/* Sidebar */}
-          <aside className="w-48 py-6.5">
-            <Nav />
-          </aside>
-
-          {/* Main Content Area */}
-          <main className="flex flex-col items-center justify-center w-full md:max-w-xl min-h-[420px] flex-1 overflow-y-auto max-md:p-4 p-8 max-md:border-t border-l border-dashed border-neutral-800">
-            {children}
-          </main>
-        </div>
+        <DemoLayout>{children}</DemoLayout>
       </body>
     </html>
   );
--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Code } from "@/components/code";
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-svh bg-red-500 text-white">
-      <Code>/loading.tsx</Code>
-    </div>
-  );
-}
