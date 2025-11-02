943d6eb4f186f72460bd606aed1edea989b5c3bf
darekrossman <darek@subpopular.dev>
2025-11-02 15:06:02 -0500

    sync

---
+++ app/demo-3/[lang]/loading.tsx
@@ -0,0 +1,9 @@
+import { Code } from "@/components/code";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-svh bg-green-500 text-white">
+      <Code>/demo-3/[lang]/loading.tsx</Code>
+    </div>
+  );
+}
