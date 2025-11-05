ef1b58a02343e743369048e9c193af15d2694b56
darekrossman <darek@subpopular.dev>
2025-11-05 12:30:04 -0500

    no cache

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,4 @@
 export async function getPost(id: string) {
-  "use cache";
   console.log("getPost", id);
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) =>
     res.json(),
