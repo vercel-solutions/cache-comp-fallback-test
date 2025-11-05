17d3a375912de6c59aba1ab79675435c9db51cec
darekrossman <darek@subpopular.dev>
2025-11-05 14:13:47 -0500

    sync

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,5 @@
 export async function getPost(id: string) {
+  "use cache";
   console.log("getPost", id);
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) =>
     res.json(),
