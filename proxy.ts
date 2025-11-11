86598c2cbbea5dcc22870af32a325778f2a121e6
darekrossman <darek@subpopular.dev>
2025-11-11 12:38:13 -0500

    proxy redirect

--- proxy.ts
+++ proxy.ts
@@ -10,7 +10,7 @@ export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   if (pathname === "/") {
-    return NextResponse.redirect(new URL(`/en/42/use-cache`, req.url));
+    return NextResponse.redirect(new URL(`/en`, req.url));
   }
 
   const response = NextResponse.next();
