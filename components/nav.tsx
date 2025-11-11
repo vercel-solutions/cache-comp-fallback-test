203e36eab88034166e943b402a3bf72308ea218f
darekrossman <darek@subpopular.dev>
2025-11-11 17:58:10 -0500

    sync

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from "react";
 import { ClearPostsCacheButton } from "./clear-tags";
 import { NavLink } from "./nav-link";
 
@@ -7,14 +6,12 @@ export async function Nav({ lang: localPromise }: { lang: Promise<string> }) {
 
   return (
     <nav className="flex flex-col h-full py-8 px-6">
-      {/* <Suspense> */}
       <div className="flex flex-col gap-5 grow">
         <NavLink href={`/${lang}/fast`}>fast posts</NavLink>
         <NavLink href={`/${lang}/slow`}>slow posts</NavLink>
       </div>
 
       <ClearPostsCacheButton />
-      {/* </Suspense> */}
     </nav>
   );
 }
