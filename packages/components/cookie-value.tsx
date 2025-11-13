2a2f0ffe539b214ab6cacaac350f8ae8c3f7d2fc
darekrossman <darek@subpopular.dev>
2025-11-13 09:50:54 -0500

    sync

--- packages/components/cookie-value.tsx
+++ packages/components/cookie-value.tsx
@@ -1,7 +1,15 @@
 import { getSessionId } from "@lib/api";
+import { cacheLife } from "next/cache";
 import { Code } from "./code";
 
 export async function CookieValue() {
+  "use cache: private";
+  cacheLife({
+    stale: 3000,
+    revalidate: 3000,
+    expire: 3000,
+  });
+
   const sessionId = await getSessionId();
 
   return (
