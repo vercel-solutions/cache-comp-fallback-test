6128f255f4eebf05a3bb686a81d11389491a5a68
darekrossman <darek@subpopular.dev>
2025-11-12 19:15:46 -0500

    sync

--- apps/ppr-no-fallbacks/app/[lang]/slow/[postId]/page.tsx
+++ apps/ppr-no-fallbacks/app/[lang]/slow/[postId]/page.tsx
@@ -40,6 +40,7 @@ export default function Page({ params }: PageProps<"/[lang]/slow/[postId]">) {
 }
 
 async function Post({ id }: { id: Promise<string> }) {
+  "use cache";
   const postId = await id;
   const post = await getPost(postId);
 
