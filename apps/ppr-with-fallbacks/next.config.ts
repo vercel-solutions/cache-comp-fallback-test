e49301c7ebd02b6a75d8e91b73237f61bec91878
darekrossman <darek@subpopular.dev>
2025-11-14 15:09:42 -0500

    sync

--- apps/ppr-with-fallbacks/next.config.ts
+++ apps/ppr-with-fallbacks/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-  // reactCompiler: true,
+  reactCompiler: true,
   transpilePackages: ["@repo/components", "@repo/lib"],
 
   experimental: {
